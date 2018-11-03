44b338c123c7fd5c5620b7f24450a83c3effa789
Kevin Lee <kevinsangholee@gmail.com>
2018-11-02 20:08:46 -0700

    first

--- project.py
+++ project.py
@@ -2,8 +2,15 @@ import tkinter as tk
 import pyautogui
 import keyboard
 
+root = tk.Tk()
+
 def move_mouse():
 	pyautogui.moveTo(100, 150)
+
+def root_loop():
+	if keyboard.is_pressed('q'):
+		print("Keypressed")
+	root.after(50, root_loop)
 
 class MainApplication(tk.Frame):
 	def __init__(self, parent, *args, **kwargs):
@@ -14,17 +21,13 @@ class MainApplication(tk.Frame):
 		button = tk.Button(self, text="Move mouse", fg="red", command=move_mouse)
 		button.pack()
 
+
+
 if __name__ == "__main__":
-	root = tk.Tk()
 	MainApplication(root).pack(side="top", fill="both", expand=True)
+	root.after(50, root_loop)
 	root.mainloop()
 	while True:
-		try:
-			if keyboard.is_pressed('q'):
-				print("Keypressed");
-				break
-			else:
-				pass
-		except:
-			break;
+		print("HELO");
+
 			
